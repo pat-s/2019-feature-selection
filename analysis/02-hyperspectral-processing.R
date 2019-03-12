4af8e0fe3c39d824e2ee25433dd36cb78412a790
pat-s <patrick.schratz@gmail.com>
2019-03-12 01:10:40 +0100

    restructure analysis

--- analysis/02-hyperspectral-processing.R
+++ analysis/02-hyperspectral-processing.R
@@ -1,12 +1,11 @@
-# crop rasters
+# crop hyperspectral rasters Mon Mar 11 22:27:14 2019 ------------------------------
+
 data_hs_preprocessed = process_hyperspec(data = data_hs_raw, plots = plot_locations,
                                          name_out = name_out, index = index,
                                          id = name_id)
 
-# create hyperspec rasters from cropped rasters
-# hyperspecs = map(data_hs_preprocessed, ~ HyperSpecRaster(.x, wavelength))
-
-# calculate various indices for all plots
+# calculate various indices for all plots Mon Mar 11 21:56:51 2019
+# ------------------------------
 ndvi_rasters = map(data_hs_preprocessed, ~ HyperSpecRaster(.x, wavelength)) %>%
   future_imap(~ vegindex(.x, "NDVI2",
                          filename = glue("data/hyperspectral/ndvi/ndvi2-{.y}"),
@@ -18,7 +17,9 @@ veg_indices = map(data_hs_preprocessed, ~ HyperSpecRaster(.x, wavelength)) %>%
 nri_indices = map(data_hs_preprocessed, ~ HyperSpecRaster(.x, wavelength)) %>%
   calc_nri_indices(indices)
 
-# extract all indices to tree data
+# extract all indices to tree data Mon Mar 11 21:58:06 2019
+# ------------------------------
+
 trees_with_indices = future_lapply(c("laukiz1", "laukiz2", "oiartzun", "luiando"), FUN = function(x) {
   extract_indices_to_plot(x, buffer = 2,
                           bf_name = "bf2",
@@ -27,9 +28,9 @@ trees_with_indices = future_lapply(c("laukiz1", "laukiz2", "oiartzun", "luiando"
                           nri_indices = nri_indices)
 })
 
-# extract all indices to tree data
-trees_with_bands = future_map(c("laukiz1", "laukiz2", "oiartzun", "luiando"), ~
-                                extract_bands_to_plot(.x, buffer = 2,
-                                                      bf_name = "bf2",
-                                                      tree_data = tree_per_tree,
-                                                      hyperspectral_bands = data_hs_preprocessed))
+trees_with_bands = future_lapply(c("laukiz1", "laukiz2", "oiartzun", "luiando"), FUN = function(x) {
+  extract_bands_to_plot(x, buffer = 2,
+                        bf_name = "bf2",
+                        tree_data = tree_per_tree,
+                        hyperspectral_bands = data_hs_preprocessed)
+})
