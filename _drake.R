9d7e86985d9c7f02000c893e3cbe81d2f80d3297
pat-s <patrick.schratz@gmail.com>
2019-03-12 01:12:52 +0100

    upd _drake.R

--- _drake.R
+++ _drake.R
@@ -1,49 +1,30 @@
-suppressPackageStartupMessages(library(drake))
-suppressPackageStartupMessages(library(hsdar))
-suppressPackageStartupMessages(library(sf))
-suppressPackageStartupMessages(library(dplyr))
-suppressPackageStartupMessages(library(purrr))
-suppressPackageStartupMessages(library(glue))
-suppressPackageStartupMessages(library(R.utils))
-suppressPackageStartupMessages(library(future))
-suppressPackageStartupMessages(library(future.callr))
-suppressPackageStartupMessages(library(future.apply))
-suppressPackageStartupMessages(library(magrittr))
-suppressPackageStartupMessages(library(ggplot2))
-suppressPackageStartupMessages(library(furrr))
-suppressPackageStartupMessages(library(data.table))
-suppressPackageStartupMessages(library(mlrCPO))
-suppressPackageStartupMessages(library(curl))
-suppressPackageStartupMessages(library(fs))
-suppressPackageStartupMessages(library(stringr))
-suppressPackageStartupMessages(library(mapview))
-suppressPackageStartupMessages(library(raster))
+source("analysis/99-packages.R")
 
 # Plans -----------------------------------------------------------
 
-data_plan = code_to_plan("analysis/data_preprocessing.R")
-download_plan = code_to_plan("analysis/download.R")
-hyperspectral_plan = code_to_plan("analysis/hyperspectral-processing.R")
-# learners = code_to_plan("code/learner/learner.R")
-# resampling = code_to_plan("code/mlr-settings/resampling.R")
-# param_set = code_to_plan("code/mlr-settings/param-set.R")
-# tune_ctrl = code_to_plan("code/mlr-settings/tune_ctrl_mbo.R")
-# tuning_wrapper = code_to_plan("code/mlr-settings/tuning.R")
-# sourceDirectory("code/benchmark/")
-# sourceDirectory("code/prediction/")
-# sourceDirectory("code/reports/")
+download_plan = code_to_plan("analysis/01-download.R")
+hyperspectral_plan = code_to_plan("analysis/02-hyperspectral-processing.R")
+data_plan = code_to_plan("analysis/04-data-preprocessing.R")
+learners = code_to_plan("analysis/05-modeling/learner.R")
+resampling = code_to_plan("analysis/05-modeling/resamp.R")
+param_set = code_to_plan("analysis/05-modeling/param-set.R")
+tune_ctrl = code_to_plan("analysis/05-modeling/tune-ctrl.R")
+tuning = code_to_plan("analysis/05-modeling/tune.R")
+train = code_to_plan("analysis/05-modeling/train.R")
 #
 sourceDirectory("R")
 
 #  grouping for visualization -----------------------------------------------------------------------
 
+download_plan$stage = "download"
+hyperspectral_plan$stage = "hyperspectral_preprocessing"
 data_plan$stage = "data"
-download_plan$stage = "data"
-hyperspectral_plan$stage = "data"
-# resampling$stage = "mlr_settings"
-# param_set$stage = "mlr_settings"
-# tune_ctrl$stage = "mlr_settings"
-# tuning_wrapper$stage = "learner"
+
+resampling$stage = "modeling"
+param_set$stage = "modeling"
+tune_ctrl$stage = "modeling"
+tuning$stage = "modeling"
+train$stage = "modeling"
 # benchmark_plan$stage = "benchmark"
 # prediction$stage = "prediction"
 # reports$stage = "reports"
@@ -52,7 +33,7 @@ hyperspectral_plan$stage = "data"
 #
 plan = bind_plans(data_plan, download_plan, hyperspectral_plan)
 
-config = drake_config(plan)
+plan %<>% mutate(stage = as.factor(stage))
 
 options(
   clustermq.scheduler = "slurm",
@@ -60,9 +41,10 @@ options(
 )
 
 # global within-target parallelism if not set specifically within a function
-plan(future.callr::callr, workers = ignore(4))
+#plan(future.callr::callr, workers = ignore(7))
 
-drake_config(plan, verbose = 2, targets = "trees_with_indices", lazy_load = "promise", console_log_file = "log/drake.log",
-             cache_log_file = "log/cache2.log",
-             caching = "worker", template = list(log_file = "log/worker%a.log", n_cpus= 16),
+drake_config(plan, verbose = 2, targets = "data_vi_nri_clean", lazy_load = "promise", console_log_file = "log/drake.log",
+             cache_log_file = "log/cache3.log",
+             caching = "worker", template = list(log_file = "log/worker%a.log", n_cpus= 32),
+             prework = quote(future::plan(future.callr::callr, workers = 3)),
              garbage_collection = TRUE, jobs = 1, parallelism = "clustermq")
