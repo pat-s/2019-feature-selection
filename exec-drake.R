185f61f311256083f07828177b398f80e2d14963
pat-s <patrick.schratz@gmail.com>
2019-04-24 15:55:58 +0200

    style

--- exec-drake.R
+++ exec-drake.R
@@ -4,11 +4,15 @@ r_make()
 
 # visualize
 
-r_vis_drake_graph(group = "stage", clusters = c("task", "learner",
-                                                "mlr_settings",
-                                                "benchmark_plan",
-                                                "prediction"),
-                  targets_only = TRUE, show_output_files = FALSE)
+r_vis_drake_graph(
+  group = "stage", clusters = c(
+    "task", "learner",
+    "mlr_settings",
+    "benchmark_plan",
+    "prediction"
+  ),
+  targets_only = TRUE, show_output_files = FALSE
+)
 
 r_vis_drake_graph()
 
@@ -21,8 +25,11 @@ r_predict_runtime()
 
 r_predict_workers()
 
-make(plan, verbose = 2, targets = "train_xgboost", lazy_load = "promise",
-     console_log_file = "log/drake.log", cache_log_file = "log/cache3.log",
-     caching = "worker", template = list(log_file = "log/worker%a.log", n_cpus= 16, memory = 120000),
-     prework = quote(future::plan(future::multisession, workers = 3)),
-     garbage_collection = TRUE, jobs = 1, parallelism = "clustermq")
+make(plan,
+  verbose = 2, targets = "data_list_nri_vi_bands", lazy_load = "promise",
+  console_log_file = "log/drake.log", cache_log_file = "log/cache3.log",
+  caching = "worker",
+  template = list(log_file = "log/worker%a.log", n_cpus = 16, memory = 120000),
+  prework = quote(future::plan(future::multisession, workers = 3)),
+  garbage_collection = TRUE, jobs = 1, parallelism = "clustermq"
+)
