20d9a5e3e1c9ae26e5bc57d7886385a5b5fef3d0
pat-s <patrick.schratz@gmail.com>
2019-04-25 17:23:38 +0200

    clean 04-data-processing.R

--- analysis/04-data-preprocessing.R
+++ analysis/04-data-preprocessing.R
@@ -1,17 +1,18 @@
-# read all datasets as list Mon Mar 11 22:28:38 2019 ------------------------------
+# read all datasets as list --------------------------------------------------------------------
 
 data_list_nri_vi_bands <- list("vi_nri" = trees_with_indices, "bands" = trees_with_bands)
 
-# clean datasets Mon Mar 11 22:28:58 2019 ------------------------------
+# clean datasets --------------------------------------------------------------------
 
 data_clean_single_plots <- map(data_list_nri_vi_bands, ~ clean_single_plots(.x))
 
-# extract coordinates of cleaned data Mon Mar 11 22:29:22 2019 ------------------------------
+# extract coordinates of cleaned data --------------------------------------------------------------------
 
 data_clean_single_plots_coords <- map(data_list_nri_vi_bands, ~ extract_coords(.x))
 
-# Merge the dataframes of vi and nri (stored in separate lists for each plot)
-# into one dataset. Possible variables with NAs are removed again, but this is
+# Merge the dataframes of vi and nri --------------------------------------------------------------------
+# (stored in separate lists for each plot) into one dataset.
+# Possible variables with NAs are removed again, but this is
 # actually happening in `clean_single_plots()`.
 
 data_vi_nri_clean <- as_tibble(rbindlist(data_clean_single_plots[[1]], fill = TRUE)) %>%
@@ -22,29 +23,32 @@ data_bands_clean <- as_tibble(rbindlist(data_clean_single_plots[[2]], fill = TRU
   Filter(function(x) !any(is.na(x)), .)
 coords_bands_clean <- as_tibble(rbindlist(data_clean_single_plots_coords[[2]]))
 
-# modify defoliation value (we get errors if defol is exactly zero) Mon Mar 11 22:31:36 2019 ------------------------------
+# modify defoliation value (we get errors if defol is exactly zero) --------------------------------------------------------------------
 
-data_trim_defoliation <- map(list(data_vi_nri_clean), ~ mutate_defol(.x)) %>%
-  set_names("data_vi_nri")
+data_trim_defoliation <- map(
+  list(data_vi_nri_clean, data_bands_clean),
+  ~ mutate_defol(.x)
+) %>%
+  set_names(c("data_vi_nri", "data_bands"))
 
-# split into feature sets Mon Mar 11 22:44:59 2019 ------------------------------
+# split into feature sets --------------------------------------------------------------------
 
-nri_data <- split_into_feature_sets(data_trim_defoliation, "nri")
+nri_data <- split_into_feature_sets(data_trim_defoliation[[1]], "nri")
 
-vi_data <- split_into_feature_sets(data_trim_defoliation, "vi")
+vi_data <- split_into_feature_sets(data_trim_defoliation[[1]], "vi")
 
-# bands_data = split_into_feature_sets(data_trim_defoliation, "bands")
+bands_data <- split_into_feature_sets(data_trim_defoliation[[2]], "bands")
 
 nri_vi_data <- cbind(nri_data, vi_data)
 
-# log transform response variable Mon Mar 11 22:45:35 2019 ------------------------------
+# log transform response variable --------------------------------------------------------------------
 
 nri_data_log_transformed <- log_response(nri_data, "defoliation")
 vi_data_log_transformed <- log_response(vi_data, "defoliation")
-# bands_data_log_transformed = log_response(bands_data, "defoliation")
+bands_data_log_transformed <- log_response(bands_data, "defoliation")
 nri_vi_data_log_transformed <- log_response(nri_vi_data, "defoliation")
 
-# create tasks Mon Mar 11 22:46:03 2019 ------------------------------
+# create tasks --------------------------------------------------------------------
 
 nri_task <- makeRegrTask(
   id = "defoliation-all-plots-NRI", data = nri_data_log_transformed,
@@ -58,12 +62,14 @@ vi_task <- makeRegrTask(
   blocking = factor(rep(1:4, c(479, 451, 300, 529)))
 )
 
-# bands_task = makeRegrTask(id = "defoliation-all-plots-bands", data = bands_data_log_transformed,
-#                           target = "defoliation", coordinates = coords_bands,
-#                           blocking = factor(rep(1:4, c(479, 451, 294, 529))))
+hr_task <- makeRegrTask(
+  id = "defoliation-all-plots-HR", data = bands_data_log_transformed,
+  target = "defoliation", coordinates = coords_bands_clean,
+  blocking = factor(rep(1:4, c(479, 451, 300, 529)))
+)
 
 nri_vi_task <- makeRegrTask(
   id = "defoliation-all-plots-NRI-VI", data = nri_vi_data_log_transformed,
-  target = "defoliation", coordinates = coords_bands,
+  target = "defoliation", coordinates = coords_vi_nri_clean,
   blocking = factor(rep(1:4, c(479, 451, 300, 529)))
 )
