a0802653c01749e849ebee7e42a8145531108c0c
pat-s <patrick.schratz@gmail.com>
2019-05-14 17:25:08 +0200

    packages and deploy script

---
+++ inst/rsync-jupiter.sh
@@ -0,0 +1,2 @@
+rsync -rlptDvzog --chown=www-data:www-data --fake-super analysis/paper/submission/3/appendices/prediction-map* -e ssh
+                   │patrick@jupiter.geogr.uni-jena.de:/home/www/life-healthy-forest/action-B1-pathogen-infection/
