a7ae8f1e8fd19fc88593435ee60e6f31e1002c04
pat-s <patrick.schratz@gmail.com>
2019-05-23 10:10:01 +0200

    wflow_publish(all = T)

--- inst/rsync-jupiter.sh
+++ inst/rsync-jupiter.sh
@@ -1,2 +1,13 @@
-rsync -rlptDvzog --chown=www-data:www-data --fake-super analysis/paper/submission/3/appendices/prediction-map* -e ssh
-                   │patrick@jupiter.geogr.uni-jena.de:/home/www/life-healthy-forest/action-B1-pathogen-infection/
+########
+# LIFE
+########
+
+# `defoliation-report` --------------- copy defoliation report to LIFE
+rsync -rlptDvzog --chown=www-data:www-data --fake-super \
+  /home/patrick/papers/2019-feature-selection/analysis/rmd/defoliation-maps.html \
+  -e ssh patrick@jupiter.geogr.uni-jena.de:/home/www/life-healthy-forest/action-B1-spatial-mapping/report-defoliation.html
+
+# `defoliation-maps` --------------- copy defoliation report to LIFE
+rsync -rlptDvzog --chown=www-data:www-data --fake-super \
+  /home/patrick/papers/2019-feature-selection/analysis/figures/defoliation-map* \
+  -e ssh patrick@jupiter.geogr.uni-jena.de:/home/www/life-healthy-forest/action-B1-spatial-mapping/defoliation-maps/
