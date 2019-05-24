b53246ddc65b702750f6b15b6142e9eb57a0c3be
pat-s <patrick.schratz@gmail.com>
2019-05-24 16:03:03 +0200

    update scripts

--- code/07-reports.R
+++ code/07-reports.R
@@ -1,16 +1,7 @@
 reports_plan = drake_plan(
 
-  # defoliation_maps = knit(
-  #   knitr_in("analysis/rmd/defoliation-maps.Rmd"),
-  #   output = file_out("analysis/rmd/defoliation-maps.md"),
-  #   quiet = TRUE),
+  defoliation_maps_wfr = wflow_publish(knitr_in("analysis/report-defoliation.Rmd"), view = FALSE),
 
-  defoliation_maps_wfr = wflow_build(knitr_in("analysis/defoliation-maps.Rmd"), view = FALSE)#,
-
-  # cloud_cover = knit(
-  #   knitr_in("analysis/rmd/cloud-cover.Rmd"),
-  #   output = file_out("analysis/rmd/cloud-cover.md"),
-  #   quiet = TRUE)
-
+  strings_in_dots = "literals"
 )
 
