9c701274ea34086a911c190cdd5ebeb5b0d0973b
pat-s <patrick.schratz@gmail.com>
2019-05-29 15:35:44 +0200

    minor

--- code/07-reports.R
+++ code/07-reports.R
@@ -1,7 +1,5 @@
 reports_plan = drake_plan(
 
-  defoliation_maps_wfr = wflow_publish(knitr_in("analysis/report-defoliation.Rmd"), view = FALSE),
-
-  strings_in_dots = "literals"
+  defoliation_maps_wfr = wflow_publish(knitr_in("analysis/report-defoliation.Rmd"), view = FALSE)
 )
 
