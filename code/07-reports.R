eaa444a9a995091aa395aff76077b954e049da3f
pat-s <patrick.schratz@gmail.com>
2019-06-15 09:14:50 +0200

    add EDA and spectral signatures report

--- code/07-reports.R
+++ code/07-reports.R
@@ -1,5 +1,9 @@
 reports_plan = drake_plan(
+  
+  eda_wfr = wflow_publish(knitr_in("analysis/eda.Rmd"), view = FALSE),
 
-  defoliation_maps_wfr = wflow_publish(knitr_in("analysis/report-defoliation.Rmd"), view = FALSE)
+  defoliation_maps_wfr = wflow_publish(knitr_in("analysis/report-defoliation.Rmd"), view = FALSE),
+  
+  spectral_signatures_wfr = wflow_publish(knitr_in("analysis/spectral-signatures.Rmd"), view = FALSE),
 )
 
