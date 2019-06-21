a40d3d2a95886baadd190dbb662980f8e8ef176e
pat-s <patrick.schratz@gmail.com>
2019-06-21 17:08:46 +0200

    use praznik mrmr

--- code/05-modeling/paper/filter-wrapper.R
+++ code/05-modeling/paper/filter-wrapper.R
@@ -3,14 +3,14 @@
 filter_wrapper_rf_borda  = makeFilterWrapper(lrn_rf, fw.method = "E-Borda", cache = TRUE,
                                              fw.base.methods = c("FSelectorRcpp_gain.ratio", "FSelectorRcpp_information.gain",
                                                                   "variance", "rank.correlation", "linear.correlation",
-                                                                  "mrmr", "praznik_CMIM", "carscore"))
+                                                                  "praznik_MRMR", "praznik_CMIM", "carscore"))
 
 filter_wrapper_rf_gain.ratio  = makeFilterWrapper(lrn_rf, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE)
 filter_wrapper_rf_info.gain  = makeFilterWrapper(lrn_rf, fw.method = "FSelectorRcpp_information.gain", cache = TRUE)
 filter_wrapper_rf_variance  = makeFilterWrapper(lrn_rf, fw.method = "variance", cache = TRUE)
 filter_wrapper_rf_rank.cor = makeFilterWrapper(lrn_rf, fw.method = "rank.correlation", cache = TRUE)
 filter_wrapper_rf_linear.cor  = makeFilterWrapper(lrn_rf, fw.method = "linear.correlation", cache = TRUE)
-filter_wrapper_rf_mrmr  = makeFilterWrapper(lrn_rf, fw.method = "mrmr", cache = TRUE)
+filter_wrapper_rf_mrmr  = makeFilterWrapper(lrn_rf, fw.method = "praznik_MRMR", cache = TRUE)
 filter_wrapper_rf_cmim  = makeFilterWrapper(lrn_rf, fw.method = "praznik_CMIM", cache = TRUE)
 filter_wrapper_rf_carscore  = makeFilterWrapper(lrn_rf, fw.method = "carscore", cache = TRUE)
 
@@ -19,14 +19,14 @@ filter_wrapper_rf_carscore  = makeFilterWrapper(lrn_rf, fw.method = "carscore", 
 filter_wrapper_xgboost_borda  = makeFilterWrapper(lrn_xgboost, fw.method = "E-Borda", cache = TRUE,
                                                   fw.base.methods = c("FSelectorRcpp_gain.ratio", "FSelectorRcpp_information.gain",
                                                                        "variance", "rank.correlation", "linear.correlation",
-                                                                       "mrmr", "praznik_CMIM", "carscore"))
+                                                                       "praznik_MRMR", "praznik_CMIM", "carscore"))
 
 filter_wrapper_xgboost_gain.ratio  = makeFilterWrapper(lrn_xgboost, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE)
 filter_wrapper_xgboost_info.gain  = makeFilterWrapper(lrn_xgboost, fw.method = "FSelectorRcpp_information.gain", cache = TRUE)
 filter_wrapper_xgboost_variance  = makeFilterWrapper(lrn_xgboost, fw.method = "variance", cache = TRUE)
 filter_wrapper_xgboost_rank.cor = makeFilterWrapper(lrn_xgboost, fw.method = "rank.correlation", cache = TRUE)
 filter_wrapper_xgboost_linear.cor  = makeFilterWrapper(lrn_xgboost, fw.method = "linear.correlation", cache = TRUE)
-filter_wrapper_xgboost_mrmr  = makeFilterWrapper(lrn_xgboost, fw.method = "mrmr", cache = TRUE)
+filter_wrapper_xgboost_mrmr  = makeFilterWrapper(lrn_xgboost, fw.method = "praznik_MRMR", cache = TRUE)
 filter_wrapper_xgboost_cmim  = makeFilterWrapper(lrn_xgboost, fw.method = "praznik_CMIM", cache = TRUE)
 filter_wrapper_xgboost_carscore  = makeFilterWrapper(lrn_xgboost, fw.method = "carscore", cache = TRUE)
 
@@ -35,14 +35,14 @@ filter_wrapper_xgboost_carscore  = makeFilterWrapper(lrn_xgboost, fw.method = "c
 filter_wrapper_svm_borda  = makeFilterWrapper(lrn_svm, fw.method = "E-Borda", cache = TRUE,
                                               fw.base.methods = c("FSelectorRcpp_gain.ratio", "FSelectorRcpp_information.gain",
                                                                    "variance", "rank.correlation", "linear.correlation",
-                                                                   "mrmr", "praznik_CMIM", "carscore"))
+                                                                   "praznik_MRMR", "praznik_CMIM", "carscore"))
 
 filter_wrapper_svm_gain.ratio  = makeFilterWrapper(lrn_svm, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE)
 filter_wrapper_svm_info.gain  = makeFilterWrapper(lrn_svm, fw.method = "FSelectorRcpp_information.gain", cache = TRUE)
 filter_wrapper_svm_variance  = makeFilterWrapper(lrn_svm, fw.method = "variance", cache = TRUE)
 filter_wrapper_svm_rank.cor = makeFilterWrapper(lrn_svm, fw.method = "rank.correlation", cache = TRUE)
 filter_wrapper_svm_linear.cor  = makeFilterWrapper(lrn_svm, fw.method = "linear.correlation", cache = TRUE)
-filter_wrapper_svm_mrmr  = makeFilterWrapper(lrn_svm, fw.method = "mrmr", cache = TRUE)
+filter_wrapper_svm_mrmr  = makeFilterWrapper(lrn_svm, fw.method = "praznik_MRMR", cache = TRUE)
 filter_wrapper_svm_cmim  = makeFilterWrapper(lrn_svm, fw.method = "praznik_CMIM", cache = TRUE)
 filter_wrapper_svm_carscore  = makeFilterWrapper(lrn_svm, fw.method = "carscore", cache = TRUE)
 
@@ -51,14 +51,14 @@ filter_wrapper_svm_carscore  = makeFilterWrapper(lrn_svm, fw.method = "carscore"
 filter_wrapper_lasso_borda  = makeFilterWrapper(lrn_lasso, fw.method = "E-Borda", cache = TRUE,
                                                 fw.base.methods = c("FSelectorRcpp_gain.ratio", "FSelectorRcpp_information.gain",
                                                                      "variance", "rank.correlation", "linear.correlation",
-                                                                     "mrmr", "praznik_CMIM", "carscore"))
+                                                                     "praznik_MRMR", "praznik_CMIM", "carscore"))
 
 filter_wrapper_lasso_gain.ratio  = makeFilterWrapper(lrn_lasso, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE)
 filter_wrapper_lasso_info.gain  = makeFilterWrapper(lrn_lasso, fw.method = "FSelectorRcpp_information.gain", cache = TRUE)
 filter_wrapper_lasso_variance  = makeFilterWrapper(lrn_lasso, fw.method = "variance", cache = TRUE)
 filter_wrapper_lasso_rank.cor = makeFilterWrapper(lrn_lasso, fw.method = "rank.correlation", cache = TRUE)
 filter_wrapper_lasso_linear.cor  = makeFilterWrapper(lrn_lasso, fw.method = "linear.correlation", cache = TRUE)
-filter_wrapper_lasso_mrmr  = makeFilterWrapper(lrn_lasso, fw.method = "mrmr", cache = TRUE)
+filter_wrapper_lasso_mrmr  = makeFilterWrapper(lrn_lasso, fw.method = "praznik_MRMR", cache = TRUE)
 filter_wrapper_lasso_cmim  = makeFilterWrapper(lrn_lasso, fw.method = "praznik_CMIM", cache = TRUE)
 filter_wrapper_lasso_carscore  = makeFilterWrapper(lrn_lasso, fw.method = "carscore", cache = TRUE)
 
@@ -67,13 +67,13 @@ filter_wrapper_lasso_carscore  = makeFilterWrapper(lrn_lasso, fw.method = "carsc
 filter_wrapper_ridge_borda  = makeFilterWrapper(lrn_ridge, fw.method = "E-Borda", cache = TRUE,
                                                 fw.base.methods = c("FSelectorRcpp_gain.ratio", "FSelectorRcpp_information.gain",
                                                                      "variance", "rank.correlation", "linear.correlation",
-                                                                     "mrmr", "praznik_CMIM", "carscore"))
+                                                                     "praznik_MRMR", "praznik_CMIM", "carscore"))
 
 filter_wrapper_ridge_gain.ratio  = makeFilterWrapper(lrn_ridge, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE)
 filter_wrapper_ridge_info.gain  = makeFilterWrapper(lrn_ridge, fw.method = "FSelectorRcpp_information.gain", cache = TRUE)
 filter_wrapper_ridge_variance  = makeFilterWrapper(lrn_ridge, fw.method = "variance", cache = TRUE)
 filter_wrapper_ridge_rank.cor = makeFilterWrapper(lrn_ridge, fw.method = "rank.correlation", cache = TRUE)
 filter_wrapper_ridge_linear.cor  = makeFilterWrapper(lrn_ridge, fw.method = "linear.correlation", cache = TRUE)
-filter_wrapper_ridge_mrmr  = makeFilterWrapper(lrn_ridge, fw.method = "mrmr", cache = TRUE)
+filter_wrapper_ridge_mrmr  = makeFilterWrapper(lrn_ridge, fw.method = "praznik_MRMR", cache = TRUE)
 filter_wrapper_ridge_cmim  = makeFilterWrapper(lrn_ridge, fw.method = "praznik_CMIM", cache = TRUE)
 filter_wrapper_ridge_carscore  = makeFilterWrapper(lrn_ridge, fw.method = "carscore", cache = TRUE)
