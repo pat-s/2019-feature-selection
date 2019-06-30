2289d0965fcd3f29c830acd5d3e53a8dcc778d00
pat-s <patrick.schratz@gmail.com>
2019-06-30 21:38:00 +0200

    upd code

--- code/06-benchmark-matrix.R
+++ code/06-benchmark-matrix.R
@@ -1,61 +1,70 @@
 # see https://ropenscilabs.github.io/drake-manual/plans.html#large-plans
 
-bm_plan = drake_plan(bm = target(
-  benchmark_custom_no_models(learner, task),
-  transform = cross(
-    task = c("vi" = vi_task, "nri" = nri_task, "hr" = hr_task),
-    learner = c(svm_borda,
-                svm_info.gain,
-                svm_gain.ratio,
-                svm_variance,
-                svm_rank.cor,
-                svm_linear.cor,
-                svm_mrmr,
-                svm_cmim,
-                svm_carscore,
-                svm_no_filter,
-                svm_pca,
+bm_plan = drake_plan(
+  bm = target(
+    benchmark_custom_no_models(learner, task),
+    transform = cross(
+      task = c("vi" = vi_task, "nri" = nri_task, "hr" = hr_task),
+      learner = c(#svm_borda,
+        svm_info.gain,
+        #svm_gain.ratio,
+        #svm_variance,
+        #svm_rank.cor,
+        svm_linear.cor,
+        svm_mrmr,
+        svm_cmim,
+        svm_carscore,
+        svm_no_filter,
+        svm_pca,
 
-                rf_borda,
-                rf_info.gain,
-                rf_gain.ratio,
-                rf_variance,
-                rf_rank.cor,
-                rf_linear.cor,
-                rf_mrmr,
-                rf_cmim,
-                rf_carscore,
-                rf_no_filter,
-                rf_pca,
+        #rf_borda,
+        rf_info.gain,
+        #rf_gain.ratio,
+        #rf_variance,
+        #rf_rank.cor,
+        rf_linear.cor,
+        rf_mrmr,
+        rf_cmim,
+        rf_carscore,
+        rf_no_filter,
+        rf_pca,
 
-                xgboost_borda,
-                xgboost_info.gain,
-                xgboost_gain.ratio,
-                xgboost_variance,
-                xgboost_rank.cor,
-                xgboost_linear.cor,
-                xgboost_mrmr,
-                xgboost_cmim,
-                xgboost_carscore,
-                xgboost_no_filter,
-                xgboost_pca,
+        #xgboost_borda,
+        xgboost_info.gain,
+        #xgboost_gain.ratio,
+        #xgboost_variance,
+        #xgboost_rank.cor,
+        xgboost_linear.cor,
+        xgboost_mrmr,
+        xgboost_cmim,
+        xgboost_carscore,
+        xgboost_no_filter,
+        xgboost_pca,
 
-                ridge_no_filter,
+        ridge_no_filter,
 
-                lasso_no_filter#,
+        lasso_no_filter#,
 
-                # lm_borda,
-                # lm_info.gain,
-                # lm_gain.ratio,
-                # lm_variance,
-                # lm_rank.cor,
-                # lm_linear.cor,
-                # lm_mrmr,
-                # lm_cmim,
-                # lm_carscore,
-                # lrn_lm,
-                # lm_pca
+        # lm_borda,
+        # lm_info.gain,
+        # lm_gain.ratio,
+        # lm_variance,
+        # lm_rank.cor,
+        # lm_linear.cor,
+        # lm_mrmr,
+        # lm_cmim,
+        # lm_carscore,
+        # lrn_lm,
+        # lm_pca
 
-                ),
-  )
-))
+      ))),
+  bm_borda = target(
+    benchmark_custom_no_models_sequential(learner, task),
+    transform = cross(
+      task = c("vi" = vi_task, "nri" = nri_task, "hr" = hr_task),
+      learner = c(svm_borda,
+                  rf_borda,
+                  xgboost_borda
+      )
+    )
+  ))
--- code/07-reports.R
+++ code/07-reports.R
@@ -6,7 +6,7 @@ reports_plan_paper = drake_plan(
 
   filter_correlations_wfr = wflow_publish(knitr_in("analysis/filter-correlation.Rmd"), view = FALSE),
 
-  eval_performance = wflow_publish(knitr_in("analysis/eval_performance.Rmd"), view = FALSE)
+  eval_performance = wflow_publish(knitr_in("analysis/eval-performance.Rmd"), view = FALSE)
 )
 
 reports_plan_project = drake_plan(
