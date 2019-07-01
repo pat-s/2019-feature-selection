24e318f34962cb4988db5581840c48721ce69b97
pat-s <patrick.schratz@gmail.com>
2019-07-01 17:02:29 +0200

    update reports

--- code/07-reports.R
+++ code/07-reports.R
@@ -1,12 +1,12 @@
 reports_plan_paper = drake_plan(
 
-  eda_wfr = wflow_publish(knitr_in("analysis/eda.Rmd"), view = FALSE),
+  eda_wfr = wflow_publish(knitr_in("analysis/eda.Rmd"), view = FALSE, verbose = TRUE),
 
   spectral_signatures_wfr = wflow_publish(knitr_in("analysis/spectral-signatures.Rmd"), view = FALSE),
 
-  filter_correlations_wfr = wflow_publish(knitr_in("analysis/filter-correlation.Rmd"), view = FALSE),
+  filter_correlations_wfr = wflow_publish(knitr_in("analysis/filter-correlation.Rmd"), view = FALSE, verbose = TRUE),
 
-  eval_performance = wflow_publish(knitr_in("analysis/eval-performance.Rmd"), view = FALSE)
+  eval_performance = wflow_publish(knitr_in("analysis/eval-performance.Rmd"), view = FALSE, verbose = TRUE)
 )
 
 reports_plan_project = drake_plan(
