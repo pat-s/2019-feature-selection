e96dfcadcede533a4dca1099ebc1d467dab6133c
pat-s <patrick.schratz@gmail.com>
2019-07-01 17:04:11 +0200

    add move figures script

---
+++ code/move-figures.R
@@ -0,0 +1,9 @@
+
+file_move("docs/figure/filter-correlation.Rmd/correlation-nbins-1.pdf",
+          "code/98-paper/journal/")
+file_move("docs/figure/filter-correlation.Rmd/correlation-filter-nri-1.pdf",
+          "code/98-paper/journal/")
+file_move("docs/figure/eda.Rmd/defoliation-distribution-plot-1.pdf",
+          "code/98-paper/journal/")
+file_move(here("docs/figure/spectral-signatures.Rmd/spectral-signatures-1.pdf"),
+          here("code/98-paper/journal/"))
