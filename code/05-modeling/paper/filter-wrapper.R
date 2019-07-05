7a60998cefb796182d8f82a8cb902e1a67031079
pat-s <patrick.schratz@gmail.com>
2019-07-05 09:42:53 +0200

    use nbins = 10 for info gain, set IDs for benchmark learners

--- code/05-modeling/paper/filter-wrapper.R
+++ code/05-modeling/paper/filter-wrapper.R
@@ -9,11 +9,11 @@ filter_wrapper_rf_borda <- makeFilterWrapper(lrn_rf,
     "carscore",
     "FSelector_relief"
   ),
-  more.args = list("FSelectorRcpp_information.gain" = list(equal = TRUE)) # FSelectorRcpp
+  more.args = list("FSelectorRcpp_information.gain" = list(equal = TRUE, nbins = 10)) # FSelectorRcpp
 )
 
-filter_wrapper_rf_gain.ratio <- makeFilterWrapper(lrn_rf, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE, equal = TRUE)
-filter_wrapper_rf_info.gain <- makeFilterWrapper(lrn_rf, fw.method = "FSelectorRcpp_information.gain", cache = TRUE, equal = TRUE)
+filter_wrapper_rf_gain.ratio <- makeFilterWrapper(lrn_rf, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE, equal = TRUE, nbins = 10)
+filter_wrapper_rf_info.gain <- makeFilterWrapper(lrn_rf, fw.method = "FSelectorRcpp_information.gain", cache = TRUE, equal = TRUE, nbins = 10)
 filter_wrapper_rf_relief <- makeFilterWrapper(lrn_rf, fw.method = "FSelector_relief", cache = TRUE)
 filter_wrapper_rf_variance <- makeFilterWrapper(lrn_rf, fw.method = "variance", cache = TRUE)
 filter_wrapper_rf_rank.cor <- makeFilterWrapper(lrn_rf, fw.method = "rank.correlation", cache = TRUE)
@@ -33,11 +33,11 @@ filter_wrapper_xgboost_borda <- makeFilterWrapper(lrn_xgboost,
     "carscore",
     "FSelector_relief"
   ),
-  more.args = list("FSelectorRcpp_information.gain" = list(equal = TRUE)) # FSelectorRcpp
+  more.args = list("FSelectorRcpp_information.gain" = list(equal = TRUE, nbins = 10)) # FSelectorRcpp
 )
 
-filter_wrapper_xgboost_gain.ratio <- makeFilterWrapper(lrn_xgboost, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE, equal = TRUE)
-filter_wrapper_xgboost_info.gain <- makeFilterWrapper(lrn_xgboost, fw.method = "FSelectorRcpp_information.gain", cache = TRUE, equal = TRUE)
+filter_wrapper_xgboost_gain.ratio <- makeFilterWrapper(lrn_xgboost, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE, equal = TRUE, nbins = 10)
+filter_wrapper_xgboost_info.gain <- makeFilterWrapper(lrn_xgboost, fw.method = "FSelectorRcpp_information.gain", cache = TRUE, equal = TRUE, nbins = 10)
 filter_wrapper_xgboost_relief <- makeFilterWrapper(lrn_xgboost, fw.method = "FSelector_relief", cache = TRUE)
 filter_wrapper_xgboost_variance <- makeFilterWrapper(lrn_xgboost, fw.method = "variance", cache = TRUE)
 filter_wrapper_xgboost_rank.cor <- makeFilterWrapper(lrn_xgboost, fw.method = "rank.correlation", cache = TRUE)
@@ -57,11 +57,11 @@ filter_wrapper_svm_borda <- makeFilterWrapper(lrn_svm,
     "carscore",
     "FSelector_relief"
   ),
-  more.args = list("FSelectorRcpp_information.gain" = list(equal = TRUE)) # FSelectorRcpp
+  more.args = list("FSelectorRcpp_information.gain" = list(equal = TRUE, nbins = 10)) # FSelectorRcpp
 )
 
-filter_wrapper_svm_gain.ratio <- makeFilterWrapper(lrn_svm, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE, equal = TRUE)
-filter_wrapper_svm_info.gain <- makeFilterWrapper(lrn_svm, fw.method = "FSelectorRcpp_information.gain", cache = TRUE, equal = TRUE)
+filter_wrapper_svm_gain.ratio <- makeFilterWrapper(lrn_svm, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE, equal = TRUE, nbins = 10)
+filter_wrapper_svm_info.gain <- makeFilterWrapper(lrn_svm, fw.method = "FSelectorRcpp_information.gain", cache = TRUE, equal = TRUE, nbins = 10)
 filter_wrapper_svm_relief <- makeFilterWrapper(lrn_svm, fw.method = "FSelector_relief", cache = TRUE)
 filter_wrapper_svm_variance <- makeFilterWrapper(lrn_svm, fw.method = "variance", cache = TRUE)
 filter_wrapper_svm_rank.cor <- makeFilterWrapper(lrn_svm, fw.method = "rank.correlation", cache = TRUE)
@@ -81,11 +81,11 @@ filter_wrapper_lasso_borda <- makeFilterWrapper(lrn_lasso,
     "carscore",
     "FSelector_relief"
   ),
-  more.args = list("FSelectorRcpp_information.gain" = list(equal = TRUE)) # FSelectorRcpp
+  more.args = list("FSelectorRcpp_information.gain" = list(equal = TRUE, nbins = 10)) # FSelectorRcpp
 )
 
-filter_wrapper_lasso_gain.ratio <- makeFilterWrapper(lrn_lasso, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE, equal = TRUE)
-filter_wrapper_lasso_info.gain <- makeFilterWrapper(lrn_lasso, fw.method = "FSelectorRcpp_information.gain", cache = TRUE, equal = TRUE)
+filter_wrapper_lasso_gain.ratio <- makeFilterWrapper(lrn_lasso, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE, equal = TRUE, nbins = 10)
+filter_wrapper_lasso_info.gain <- makeFilterWrapper(lrn_lasso, fw.method = "FSelectorRcpp_information.gain", cache = TRUE, equal = TRUE, nbins = 10)
 filter_wrapper_lasso_variance <- makeFilterWrapper(lrn_lasso, fw.method = "variance", cache = TRUE)
 filter_wrapper_lasso_rank.cor <- makeFilterWrapper(lrn_lasso, fw.method = "rank.correlation", cache = TRUE)
 filter_wrapper_lasso_linear.cor <- makeFilterWrapper(lrn_lasso, fw.method = "linear.correlation", cache = TRUE)
@@ -104,11 +104,11 @@ filter_wrapper_ridge_borda <- makeFilterWrapper(lrn_ridge,
     "carscore",
     "FSelector_relief"
   ),
-  more.args = list("FSelectorRcpp_information.gain" = list(equal = TRUE)) # FSelectorRcpp
+  more.args = list("FSelectorRcpp_information.gain" = list(equal = TRUE, nbins = 10)) # FSelectorRcpp
 )
 
-filter_wrapper_ridge_gain.ratio <- makeFilterWrapper(lrn_ridge, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE, equal = TRUE)
-filter_wrapper_ridge_info.gain <- makeFilterWrapper(lrn_ridge, fw.method = "FSelectorRcpp_information.gain", cache = TRUE, equal = TRUE)
+filter_wrapper_ridge_gain.ratio <- makeFilterWrapper(lrn_ridge, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE, equal = TRUE, nbins = 10)
+filter_wrapper_ridge_info.gain <- makeFilterWrapper(lrn_ridge, fw.method = "FSelectorRcpp_information.gain", cache = TRUE, equal = TRUE, nbins = 10)
 filter_wrapper_ridge_variance <- makeFilterWrapper(lrn_ridge, fw.method = "variance", cache = TRUE)
 filter_wrapper_ridge_rank.cor <- makeFilterWrapper(lrn_ridge, fw.method = "rank.correlation", cache = TRUE)
 filter_wrapper_ridge_linear.cor <- makeFilterWrapper(lrn_ridge, fw.method = "linear.correlation", cache = TRUE)
@@ -127,11 +127,11 @@ filter_wrapper_lm_borda <- makeFilterWrapper(lrn_lm,
     "carscore",
     "FSelector_relief"
   ),
-  equal = TRUE # FSelectorRcpp
+  equal = TRUE, nbins = 10 # FSelectorRcpp
 )
 
-filter_wrapper_lm_gain.ratio <- makeFilterWrapper(lrn_lm, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE, equal = TRUE)
-filter_wrapper_lm_info.gain <- makeFilterWrapper(lrn_lm, fw.method = "FSelectorRcpp_information.gain", cache = TRUE, equal = TRUE)
+filter_wrapper_lm_gain.ratio <- makeFilterWrapper(lrn_lm, fw.method = "FSelectorRcpp_gain.ratio", cache = TRUE, equal = TRUE, nbins = 10)
+filter_wrapper_lm_info.gain <- makeFilterWrapper(lrn_lm, fw.method = "FSelectorRcpp_information.gain", cache = TRUE, equal = TRUE, nbins = 10)
 filter_wrapper_lm_variance <- makeFilterWrapper(lrn_lm, fw.method = "variance", cache = TRUE)
 filter_wrapper_lm_rank.cor <- makeFilterWrapper(lrn_lm, fw.method = "rank.correlation", cache = TRUE)
 filter_wrapper_lm_linear.cor <- makeFilterWrapper(lrn_lm, fw.method = "linear.correlation", cache = TRUE)
--- code/05-modeling/paper/tune-wrapper.R
+++ code/05-modeling/paper/tune-wrapper.R
@@ -3,186 +3,222 @@
 svm_borda = makeTuneWrapper(filter_wrapper_svm_borda, resampling = inner,
                             par.set = ps_svm_filter,
                             control = tune.ctrl_svm, show.info = TRUE,
-                            measures = list(rmse))
+                            measures = list(rmse)) %>%
+  magrittr::inset("id", "SVM Borda")
 
 svm_info.gain = makeTuneWrapper(filter_wrapper_svm_info.gain, resampling = inner,
                                 par.set = ps_svm_filter,
                                 control = tune.ctrl_svm, show.info = TRUE,
-                                measures = list(rmse))
+                                measures = list(rmse)) %>%
+  magrittr::inset("id", "SVM Info Gain")
 
 svm_gain.ratio = makeTuneWrapper(filter_wrapper_svm_gain.ratio, resampling = inner,
                                  par.set = ps_svm_filter,
                                  control = tune.ctrl_svm, show.info = TRUE,
-                                 measures = list(rmse))
+                                 measures = list(rmse)) %>%
+  magrittr::inset("id", "SVM Gain Ratio")
 
 svm_relief = makeTuneWrapper(filter_wrapper_svm_relief, resampling = inner,
                              par.set = ps_svm_filter,
                              control = tune.ctrl_svm, show.info = TRUE,
-                             measures = list(rmse))
+                             measures = list(rmse)) %>%
+  magrittr::inset("id", "SVM Relief")
 
 svm_variance = makeTuneWrapper(filter_wrapper_svm_variance, resampling = inner,
                                par.set = ps_svm_filter,
                                control = tune.ctrl_svm, show.info = TRUE,
-                               measures = list(rmse))
+                               measures = list(rmse)) %>%
+  magrittr::inset("id", "SVM Variance")
 
 svm_rank.cor = makeTuneWrapper(filter_wrapper_svm_rank.cor, resampling = inner,
                                par.set = ps_svm_filter,
                                control = tune.ctrl_svm, show.info = TRUE,
-                               measures = list(rmse))
+                               measures = list(rmse)) %>%
+  magrittr::inset("id", "SVM Spearman")
 
 svm_linear.cor = makeTuneWrapper(filter_wrapper_svm_linear.cor, resampling = inner,
                                  par.set = ps_svm_filter,
                                  control = tune.ctrl_svm, show.info = TRUE,
-                                 measures = list(rmse))
+                                 measures = list(rmse)) %>%
+  magrittr::inset("id", "SVM Pearson")
 
 svm_mrmr = makeTuneWrapper(filter_wrapper_svm_mrmr, resampling = inner,
                            par.set = ps_svm_filter,
                            control = tune.ctrl_svm, show.info = TRUE,
-                           measures = list(rmse))
+                           measures = list(rmse)) %>%
+  magrittr::inset("id", "SVM MRMR")
 
 svm_cmim = makeTuneWrapper(filter_wrapper_svm_cmim, resampling = inner,
                            par.set = ps_svm_filter,
                            control = tune.ctrl_svm, show.info = TRUE,
-                           measures = list(rmse))
+                           measures = list(rmse)) %>%
+  magrittr::inset("id", "SVM CMIM")
 
 svm_carscore = makeTuneWrapper(filter_wrapper_svm_carscore, resampling = inner,
                                par.set = ps_svm_filter,
                                control = tune.ctrl_svm, show.info = TRUE,
-                               measures = list(rmse))
+                               measures = list(rmse)) %>%
+  magrittr::inset("id", "SVM Car")
 
 svm_no_filter = makeTuneWrapper(lrn_svm, resampling = inner,
                                 par.set = ps_svm,
                                 control = tune.ctrl_svm, show.info = TRUE,
-                                measures = list(rmse))
+                                measures = list(rmse)) %>%
+  magrittr::inset("id", "SVM")
 
 svm_pca = makeTuneWrapper(pca_wrapper_svm, resampling = inner,
                           par.set = ps_svm_pca,
                           control = tune.ctrl_svm, show.info = TRUE,
-                          measures = list(rmse))
+                          measures = list(rmse)) %>%
+  magrittr::inset("id", "SVM PCA")
 
 # XGBOOST -----------------------------------------------------------------
 
 xgboost_borda = makeTuneWrapper(filter_wrapper_xgboost_borda, resampling = inner,
                                 par.set = ps_xgboost_filter,
                                 control = tune.ctrl_xgboost,
-                                show.info = TRUE, measures = list(rmse))
+                                show.info = TRUE, measures = list(rmse)) %>%
+  magrittr::inset("id", "XG Borda")
 
 xgboost_info.gain = makeTuneWrapper(filter_wrapper_xgboost_info.gain, resampling = inner,
                                     par.set = ps_xgboost_filter,
                                     control = tune.ctrl_xgboost, show.info = TRUE,
-                                    measures = list(rmse))
+                                    measures = list(rmse)) %>%
+  magrittr::inset("id", "XG Info Gain")
 
 xgboost_gain.ratio = makeTuneWrapper(filter_wrapper_xgboost_gain.ratio, resampling = inner,
                                      par.set = ps_xgboost_filter,
                                      control = tune.ctrl_xgboost, show.info = TRUE,
-                                     measures = list(rmse))
+                                     measures = list(rmse))%>%
+  magrittr::inset("id", "XG Gain Ratio")
 
 xgboost_relief = makeTuneWrapper(filter_wrapper_xgboost_relief, resampling = inner,
                                  par.set = ps_xgboost_filter,
                                  control = tune.ctrl_xgboost, show.info = TRUE,
-                                 measures = list(rmse))
+                                 measures = list(rmse)) %>%
+  magrittr::inset("id", "XG Relief")
 
 xgboost_variance = makeTuneWrapper(filter_wrapper_xgboost_variance, resampling = inner,
                                    par.set = ps_xgboost_filter,
                                    control = tune.ctrl_xgboost, show.info = TRUE,
-                                   measures = list(rmse))
+                                   measures = list(rmse)) %>%
+  magrittr::inset("id", "XG Variance")
 
 xgboost_rank.cor = makeTuneWrapper(filter_wrapper_xgboost_rank.cor, resampling = inner,
                                    par.set = ps_xgboost_filter,
                                    control = tune.ctrl_xgboost, show.info = TRUE,
-                                   measures = list(rmse))
+                                   measures = list(rmse)) %>%
+  magrittr::inset("id", "XG Spearman")
 
 xgboost_linear.cor = makeTuneWrapper(filter_wrapper_xgboost_linear.cor, resampling = inner,
                                      par.set = ps_xgboost_filter,
                                      control = tune.ctrl_xgboost, show.info = TRUE,
-                                     measures = list(rmse))
+                                     measures = list(rmse)) %>%
+  magrittr::inset("id", "XG Pearson")
 
 xgboost_mrmr = makeTuneWrapper(filter_wrapper_xgboost_mrmr, resampling = inner,
                                par.set = ps_xgboost_filter,
                                control = tune.ctrl_xgboost, show.info = TRUE,
-                               measures = list(rmse))
+                               measures = list(rmse)) %>%
+  magrittr::inset("id", "XG MRMR")
 
 xgboost_cmim = makeTuneWrapper(filter_wrapper_xgboost_cmim, resampling = inner,
                                par.set = ps_xgboost_filter,
                                control = tune.ctrl_xgboost, show.info = TRUE,
-                               measures = list(rmse))
+                               measures = list(rmse)) %>%
+  magrittr::inset("id", "XG CMIM")
 
 xgboost_carscore = makeTuneWrapper(filter_wrapper_xgboost_carscore, resampling = inner,
                                    par.set = ps_xgboost_filter,
                                    control = tune.ctrl_xgboost, show.info = TRUE,
-                                   measures = list(rmse))
+                                   measures = list(rmse)) %>%
+  magrittr::inset("id", "XG Car")
 
 xgboost_no_filter = makeTuneWrapper(lrn_xgboost, resampling = inner,
                                     par.set = ps_xgboost,
                                     control = tune.ctrl_xgboost, show.info = TRUE,
-                                    measures = list(rmse))
+                                    measures = list(rmse)) %>%
+  magrittr::inset("id", "XG")
 
 xgboost_pca = makeTuneWrapper(pca_wrapper_xgboost, resampling = inner,
                               par.set = ps_xgboost_pca,
                               control = tune.ctrl_xgboost, show.info = TRUE,
-                              measures = list(rmse))
+                              measures = list(rmse)) %>%
+  magrittr::inset("id", "XG PCA")
 
 # Random Forest -----------------------------------------------------------
 
 rf_borda = makeTuneWrapper(filter_wrapper_rf_borda, resampling = inner,
                            par.set = ps_rf_filter,
                            control = tune.ctrl_rf, show.info = TRUE,
-                           measures = list(rmse))
+                           measures = list(rmse)) %>%
+  magrittr::inset("id", "RF Borda")
 
 rf_info.gain = makeTuneWrapper(filter_wrapper_rf_info.gain, resampling = inner,
                                par.set = ps_rf_filter,
                                control = tune.ctrl_rf, show.info = TRUE,
-                               measures = list(rmse))
+                               measures = list(rmse))  %>%
+  magrittr::inset("id", "RF Info Gain")
 
 rf_gain.ratio = makeTuneWrapper(filter_wrapper_rf_gain.ratio, resampling = inner,
                                 par.set = ps_rf_filter,
                                 control = tune.ctrl_rf, show.info = TRUE,
-                                measures = list(rmse))
+                                measures = list(rmse))  %>%
+  magrittr::inset("id", "RF Gain Ratio")
 
 rf_relief = makeTuneWrapper(filter_wrapper_rf_relief, resampling = inner,
                             par.set = ps_rf_filter,
                             control = tune.ctrl_rf, show.info = TRUE,
-                            measures = list(rmse))
+                            measures = list(rmse))  %>%
+  magrittr::inset("id", "RF Relief")
 
 rf_variance = makeTuneWrapper(filter_wrapper_rf_variance, resampling = inner,
                               par.set = ps_rf_filter,
                               control = tune.ctrl_rf, show.info = TRUE,
-                              measures = list(rmse))
+                              measures = list(rmse))  %>%
+  magrittr::inset("id", "RF Variance")
 
 rf_rank.cor = makeTuneWrapper(filter_wrapper_rf_rank.cor, resampling = inner,
                               par.set = ps_rf_filter,
                               control = tune.ctrl_rf, show.info = TRUE,
-                              measures = list(rmse))
+                              measures = list(rmse))  %>%
+  magrittr::inset("id", "RF Spearman")
 
 rf_linear.cor = makeTuneWrapper(filter_wrapper_rf_linear.cor, resampling = inner,
                                 par.set = ps_rf_filter,
                                 control = tune.ctrl_rf, show.info = TRUE,
-                                measures = list(rmse))
+                                measures = list(rmse))  %>%
+  magrittr::inset("id", "RF Pearson")
 
 rf_mrmr = makeTuneWrapper(filter_wrapper_rf_mrmr, resampling = inner,
                           par.set = ps_rf_filter,
                           control = tune.ctrl_rf, show.info = TRUE,
-                          measures = list(rmse))
+                          measures = list(rmse))  %>%
+  magrittr::inset("id", "RF MRMR")
 
 rf_cmim = makeTuneWrapper(filter_wrapper_rf_cmim, resampling = inner,
                           par.set = ps_rf_filter,
                           control = tune.ctrl_rf, show.info = TRUE,
-                          measures = list(rmse))
+                          measures = list(rmse))  %>%
+  magrittr::inset("id", "RF CMIM")
 
 rf_carscore = makeTuneWrapper(filter_wrapper_rf_carscore, resampling = inner,
                               par.set = ps_rf_filter,
                               control = tune.ctrl_rf, show.info = TRUE,
-                              measures = list(rmse))
+                              measures = list(rmse))  %>%
+  magrittr::inset("id", "RF Car")
 
 rf_no_filter = makeTuneWrapper(lrn_rf, resampling = inner,
                                par.set = ps_rf,
                                control = tune.ctrl_rf, show.info = TRUE,
-                               measures = list(rmse))
+                               measures = list(rmse))  %>%
+  magrittr::inset("id", "RF")
 
 rf_pca = makeTuneWrapper(pca_wrapper_rf, resampling = inner,
                          par.set = ps_rf_pca,
                          control = tune.ctrl_rf, show.info = TRUE,
-                         measures = list(rmse))
+                         measures = list(rmse))  %>%
+  magrittr::inset("id", "RF PCA")
 
 # RIDGE ---------------------------------------------------------------------
 
@@ -234,7 +270,8 @@ ridge_carscore = makeTuneWrapper(filter_wrapper_ridge_carscore, resampling = inn
 ridge_no_filter = makeTuneWrapper(lrn_ridge, resampling = inner,
                                   par.set = ps_ridge,
                                   control = tune.ctrl_ridge, show.info = TRUE,
-                                  measures = list(rmse))
+                                  measures = list(rmse))  %>%
+  magrittr::inset("id", "RIDGE")
 
 ridge_pca = makeTuneWrapper(pca_wrapper_ridge, resampling = inner,
                             par.set = ps_ridge_pca,
@@ -291,7 +328,8 @@ lasso_carscore = makeTuneWrapper(filter_wrapper_lasso_carscore, resampling = inn
 lasso_no_filter = makeTuneWrapper(lrn_lasso, resampling = inner,
                                   par.set = ps_lasso,
                                   control = tune.ctrl_lasso, show.info = TRUE,
-                                  measures = list(rmse))
+                                  measures = list(rmse))  %>%
+  magrittr::inset("id", "LASSO")
 
 lasso_pca = makeTuneWrapper(pca_wrapper_lasso, resampling = inner,
                             par.set = ps_lasso_pca,
