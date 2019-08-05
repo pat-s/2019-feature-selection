b28f744cfd768e74f98552dd491328877701d83d
pat-s <patrick.schratz@gmail.com>
2019-08-05 17:46:23 +0200

    add code for feature importance generation and filter values

--- code/05-modeling/paper/feature-importance.R
+++ code/05-modeling/paper/feature-importance.R
@@ -1,9 +1,10 @@
-fi_permut_vi = generateFilterValuesData(vi_task, method = "permutation.importance",
-                                        imp.learner = "regr.ksvm", nmc = 100,
+fi_permut_vi = feature_imp_parallel(vi_task, learner = "regr.ksvm", nmc = 25, cpus = 25,
+                                    measure = rmse)
+fi_permut_nri = feature_imp_parallel(nri_task, learner = "regr.ksvm", nmc = 25, cpus = 25,
+                                     measure = rmse)
+fi_permut_hr_vi = feature_imp_parallel(hr_vi_task, learner = "regr.ksvm", nmc = 25, cpus = 25,
+                                       measure = rmse)
+fi_permut_hr_nri = feature_imp_parallel(hr_nri_task, learner = "regr.ksvm", nmc = 25, cpus = 25,
                                         measure = rmse)
-fi_permut_nri = generateFilterValuesData(nri_task, method = "permutation.importance",
-                                         imp.learner = "regr.ksvm", nmc = 10,
-                                         measure = rmse)
-fi_permut_hr = generateFilterValuesData(hr_task, method = "permutation.importance",
-                                        imp.learner = "regr.ksvm", nmc = 100,
-                                        measure = rmse)
+fi_permut_hr_nri_vi = feature_imp_parallel(hr_nri_vi_task, learner = "regr.ksvm", nmc = 25, cpus = 25,
+                                           measure = rmse)
