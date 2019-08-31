ac30ccd2b419fd63ea59abd5f1ac7624de182725
pat-s <patrick.schratz@gmail.com>
2019-08-31 14:26:55 +0200

    update custom functions

--- R/06-mlr-paper.R
+++ R/06-mlr-paper.R
@@ -27,6 +27,28 @@ benchmark_custom_no_models <- function(learner, task) {
   return(bmr)
 }
 
+benchmark_custom <- function(learner, task) {
+
+  parallelStart(
+    mode = "multicore",
+    level = "mlr.resample",
+    cpus = 4
+  )
+  set.seed(12345, kind = "L'Ecuyer-CMRG")
+
+  bmr <- benchmark(learners = learner,
+                   tasks = task,
+                   models = TRUE,
+                   keep.pred = TRUE,
+                   resamplings = makeResampleDesc("CV", fixed = TRUE),
+                   show.info = TRUE,
+                   measures = list(rmse, timetrain)
+  )
+
+  parallelStop()
+  return(bmr)
+}
+
 benchmark_custom_no_models_sequential <- function(learner, task) {
 
   set.seed(12345, kind = "L'Ecuyer-CMRG")
@@ -43,20 +65,11 @@ benchmark_custom_no_models_sequential <- function(learner, task) {
   return(bmr)
 }
 
-#' @title Parallel feature importance wrapper
-#' @description Calculates feature importance via permutation
-feature_imp_parallel = function(task, learner, nmc, cpus, measure) {
+inv_boxcox_rmse = function(truth, response) {
 
-  parallelStart(
-    mode = "multicore",
-    cpus = cpus
-  )
-  set.seed(12345, kind = "L'Ecuyer-CMRG")
+  lambda = 0.7878788
+  truth = (lambda*y_new1 + 1)^(1/lambda)
+  response = (lambda*y_new1 + 1)^(1/lambda)
 
-  fi = generateFeatureImportanceData(task = task, method = "permutation.importance",
-                                     learner = learner, nmc = nmc, local = FALSE,
-                                     measure = measure, show.info = TRUE
-  )
-  parallelStop()
-  return(fi)
+  sqrt(measureMSE(truth, response))
 }
