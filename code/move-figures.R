633e0ee7bac1f16a633da040332323d8432d919e
pat-s <patrick.schratz@gmail.com>
2019-10-03 10:44:14 +0200

    upd move-figures.R

--- code/move-figures.R
+++ code/move-figures.R
@@ -14,11 +14,17 @@ file_move(
   here("docs/figure/spectral-signatures.Rmd/spectral-signatures-1.pdf"),
   here("code/98-paper/ieee/pdf")
 )
+
+## eval-performance
 file_move(
   here("docs/figure/eval-performance.Rmd/performance-results-1.pdf"),
   here("code/98-paper/ieee/pdf")
 )
 file_move(
   here("docs/figure/eval-performance.Rmd/filter-effect-1.pdf"),
+  here("code/98-paper/ieee/pdf")
+)
+file_move(
+  here("docs/figure/eval-performance.Rmd/filter-perf-all-1.pdf"),
   here("code/98-paper/ieee/pdf")
 )
