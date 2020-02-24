52640924041483d14cb67ba99d43bac90edd3139
pat-s <patrick.schratz@gmail.com>
2020-02-24 16:43:52 +0100

    rework hyperspectral extraction

--- code/02-hyperspectral-processing.R
+++ code/02-hyperspectral-processing.R
@@ -1,5 +1,4 @@
 hyperspectra_processing_plan <- drake_plan(
-
   data_hs_preprocessed = target(
     process_hyperspec(
       data = data_hs_raw, plots = plot_locations,
@@ -30,13 +29,14 @@ hyperspectra_processing_plan <- drake_plan(
     lapply(c("laukiz1", "laukiz2", "luiando", "oiartzun"),
       FUN = function(x) {
         extract_indices_to_plot(x,
-          buffer = 2,
+          buffer = buffer,
           tree_data = tree_per_tree,
           veg_indices = veg_indices,
           nri_indices = nri_indices
         )
       }
-    )
+    ),
+    transform = map(buffer = c(0, 1, 1.5, 2))
   ),
 
   trees_with_bands = target(
@@ -48,33 +48,76 @@ hyperspectra_processing_plan <- drake_plan(
           hyperspectral_bands = data_hs_preprocessed
         )
       }
-    )
+    ),
+    transform = map(buffer = c(0, 1, 1.5, 2))
   ),
 
   # corrected data from Jan 2020 -----------------------------------------------
 
-  trees_with_indices_corrected = target(
-    lapply(c("laukiz1", "laukiz2", "luiando", "oiartzun"),
-      FUN = function(x) {
-        extract_indices_to_plot(x,
-          buffer = 2,
-          tree_data = tree_per_tree_corrected,
-          veg_indices = veg_indices,
-          nri_indices = nri_indices
-        )
-      }
+  # trees_with_indices_corrected_buffer2 = target(
+  #   lapply(c("laukiz1", "laukiz2", "luiando", "oiartzun"),
+  #     FUN = function(x) {
+  #       extract_indices_to_plot(x,
+  #         buffer = 2,
+  #         tree_data = tree_per_tree_corrected,
+  #         veg_indices = veg_indices,
+  #         nri_indices = nri_indices
+  #       )
+  #     }
+  #   ),
+  #   transform = map(buffer = c(0, 1, 1.5, 2))
+  # ),
+
+  buffer = c(1, 2, 3),
+  plot_names = c("laukiz1", "laukiz2", "luiando", "oiartzun"),
+
+  trees_with_indices_corrected_buffer2 = target(
+    list(
+      extract_indices_to_plot(plot_names,
+        buffer = buffer,
+        tree_data = tree_per_tree_corrected,
+        veg_indices = veg_indices,
+        nri_indices = nri_indices
+      )
+    ),
+    dynamic = cross(
+      buffer, plot_names
     )
   ),
 
-  trees_with_bands_corrected = target(
-    lapply(c("laukiz1", "laukiz2", "luiando", "oiartzun"),
-      FUN = function(x) {
-        extract_bands_to_plot(x,
-          buffer = 2,
-          tree_data = tree_per_tree_corrected,
-          hyperspectral_bands = data_hs_preprocessed
-        )
-      }
+  trees_with_indices_corrected_no_buffer = target(
+    list(
+      extract_indices_to_plot(plot_names,
+        buffer = NULL,
+        tree_data = tree_per_tree_corrected,
+        veg_indices = veg_indices,
+        nri_indices = nri_indices
+      )
+    ),
+    dynamic = map(plot_names)
+  ),
+
+  trees_with_bands_corrected_buffer2 = target(
+    list(
+      extract_bands_to_plot(plot_names,
+        buffer = buffer,
+        tree_data = tree_per_tree_corrected,
+        hyperspectral_bands = data_hs_preprocessed
+      )
+    ),
+    dynamic = cross(
+      buffer, plot_names
     )
-  )
+  ),
+
+  trees_with_bands_corrected_no_buffer = target(
+    list(
+      extract_bands_to_plot(plot_names,
+        buffer = NULL,
+        tree_data = tree_per_tree_corrected,
+        hyperspectral_bands = data_hs_preprocessed
+      )
+    ),
+    dynamic = map(plot_names)
+  ),
 )
