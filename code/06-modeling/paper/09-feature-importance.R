d91a2fa5d4802883b4c2e4158048ec4650165c01
pat-s <patrick.schratz@gmail.com>
2020-02-24 22:33:45 +0100

    upd 09-feat imp.R

--- code/06-modeling/paper/09-feature-importance.R
+++ code/06-modeling/paper/09-feature-importance.R
@@ -1,5 +1,5 @@
 feature_imp_plan <- drake_plan(
-  fi_permut_hr = target(feature_imp_parallel(task_new_hr,
+  fi_permut_hr_buffer2 = target(feature_imp_parallel(task_new_buffer2[[1]],
     learner = "regr.ksvm", nmc = 100,
     measure = list(
       setAggregation(rmse, test.mean)
