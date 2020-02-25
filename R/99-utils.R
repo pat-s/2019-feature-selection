b6b15aefa96294a0da2a70b07f954c4d94de5466
pat-s <patrick.schratz@gmail.com>
2020-02-25 16:16:23 +0100

    fix file_cp_ts()

--- R/99-utils.R
+++ R/99-utils.R
@@ -17,28 +17,72 @@
 #' # now both files are equal, no copying will be done
 #' file_cp_ts("test.xy", "log/")
 file_cp_ts <- function(file_in, dir_out) {
+  if (!fs::file_exists(file_in)) {
+    cli::cli_alert_info("File {.file {file_in}} does not exist. Skipping.")
+    return()
+  }
+
   basename <- fs::path_file(file_in)
   file_out <- glue::glue("{dir_out}{basename}")
 
-  # if (!file_exists(glue::glue("{dir_out}{basename}"))) {
-  #   stop(sprintf("'%s' does not exist."))
-  # }
-
-  # just in case create output dir if it does not exist
+  # create output dir if it does not exist
   fs::dir_create(dir_out)
 
-  if (file_exists(file_out)) {
+  if (fs::file_exists(file_out)) {
     file_in_time <- lubridate::as_datetime(fs::file_info(file_in)$modification_time)
     file_out_time <- lubridate::as_datetime(fs::file_info(file_out)$modification_time)
 
     if (file_in_time > file_out_time) {
       fs::file_copy(file_in, file_out, overwrite = TRUE)
       cli::cli_alert_info("Timestamp of file {.file {basename}} newer than in
-      destination, copying file.", wrap = TRUE)
+                          destination, copying file.", wrap = TRUE)
     } else {
       cli::cli_alert_success("{.file {basename}}: Everything up-to-date.")
     }
   } else {
     fs::file_copy(file_in, file_out)
   }
+}
+
+copy_figures <- function() {
+
+  # eda.Rmd --------------------------------------------------------------------
+
+  file_cp_ts(
+    here::here("docs/figure/eda.Rmd/defoliation-distribution-plot-1.pdf"),
+    here::here("docs/00-manuscripts/ieee/pdf/")
+  )
+
+  # spectral-signatures.Rmd ----------------------------------------------------
+
+  file_cp_ts(
+    here::here("docs/figure/spectral-signatures.Rmd/spectral-signatures-1.pdf"),
+    here::here("docs/00-manuscripts/ieee/pdf/")
+  )
+
+  # eval-performance.Rmd -------------------------------------------------------
+
+  file_cp_ts(
+    here::here("docs/figure/eval-performance.Rmd/performance-results-1.pdf"),
+    here::here("docs/00-manuscripts/ieee/pdf/")
+  )
+  # file_cp_ts(
+  #   "docs/figure/eval-performance.Rmd/filter-effect-1.pdf",
+  #   "docs/00-manuscripts/ieee/pdf/"
+  # )
+  # file_cp_ts(
+  #   "docs/figure/eval-performance.Rmd/filter-perf-all-1.pdf",
+  #   "docs/00-manuscripts/ieee/pdf/"
+  # )
+
+  # filter-correlation.Rmd -----------------------------------------------------
+
+  # file_cp_ts(
+  #   "docs/figure/filter-correlation.Rmd/correlation-nbins-1.pdf",
+  #   "docs/00-manuscripts/ieee/pdf/"
+  # )
+  # file_cp_ts(
+  #   "docs/figure/filter-correlation.Rmd/correlation-filter-nri-1.pdf",
+  #   "docs/00-manuscripts/ieee/pdf/"
+  # )
 }
