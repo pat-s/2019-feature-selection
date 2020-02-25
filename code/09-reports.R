426c7405d613c19d2426f378cc19572be953af3a
pat-s <patrick.schratz@gmail.com>
2020-02-25 16:18:32 +0100

    refactor 09-reports

--- code/09-reports.R
+++ code/09-reports.R
@@ -1,12 +1,37 @@
 reports_plan_paper <- drake_plan(
-
-  eda_wfr = wflow_publish(knitr_in("analysis/eda.Rmd"), view = FALSE, verbose = TRUE),
-  spectral_signatures_wfr = wflow_publish(knitr_in("analysis/spectral-signatures.Rmd"), view = FALSE),
-  filter_correlations_wfr = wflow_publish(knitr_in("analysis/filter-correlation.Rmd"), view = FALSE, verbose = TRUE),
-  eval_performance = wflow_publish(knitr_in("analysis/eval-performance.Rmd"), view = FALSE, verbose = TRUE),
-  response_normality = wflow_publish(knitr_in("analysis/response-normality.Rmd"), view = FALSE, verbose = TRUE)
+  eda_wfr = target({
+    wflow_publish(knitr_in("analysis/eda.Rmd"), view = FALSE, verbose = TRUE)
+    copy_figures()
+  }),
+  spectral_signatures_wfr = target({
+    wflow_publish(knitr_in("analysis/spectral-signatures.Rmd"), view = FALSE)
+    copy_figures()
+  }),
+  filter_correlations_wfr = target({
+    wflow_publish(knitr_in("analysis/filter-correlation.Rmd"),
+      view = FALSE,
+      verbose = TRUE
+    )
+    copy_figures()
+  }),
+  eval_performance_wfr = target({
+    wflow_publish(knitr_in("analysis/eval-performance.Rmd"),
+      view = FALSE,
+      verbose = TRUE
+    )
+    copy_figures()
+  }),
+  response_normality_wfr = target({
+    wflow_publish(knitr_in("analysis/response-normality.Rmd"),
+      view = FALSE,
+      verbose = TRUE
+    )
+    copy_figures()
+  })
 )
 
 reports_plan_project <- drake_plan(
-  defoliation_maps_wfr = wflow_publish(knitr_in("analysis/report-defoliation.Rmd"), view = FALSE)
+  defoliation_maps_wfr = target(
+    wflow_publish(knitr_in("analysis/report-defoliation.Rmd"), view = FALSE)
+  )
 )
