e21ba13d6ca177dd205f173fefd6db7067f92cdf
pat-s <patrick.schratz@gmail.com>
2020-02-28 13:44:27 +0100

    fix targets

--- code/082-aggregate_no_buffer.R
+++ code/082-aggregate_no_buffer.R
@@ -1,12 +1,12 @@
 bm_aggregated_plan_no_buffer <- drake_plan(
   bm_aggregated_temp1_no_buffer = target(
     mergeBenchmarkResults(
-      benchmark_no_models_new
+      benchmark_no_models_new_no_buffer
     )
   ),
   bm_aggregated_temp2_no_buffer = target(
     mergeBenchmarkResults(
-      benchmark_no_models_new_penalized
+      benchmark_no_models_new_penalized_no_buffer
     )
   ),
   bm_aggregated_new_no_buffer = target(
