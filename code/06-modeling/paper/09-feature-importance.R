6861c59d9476de77da99eb0bb4f799463731e838
pat-s <patrick.schratz@gmail.com>
2020-03-03 14:56:14 +0100

    calculate wavelengths from veg indices

--- code/06-modeling/paper/09-feature-importance.R
+++ code/06-modeling/paper/09-feature-importance.R
@@ -12,4 +12,49 @@ feature_imp_plan <- drake_plan(
       setAggregation(rmse, test.mean)
     )
   )),
+
+  df_wavelengths_from_indices = target({
+
+    # The table is a cleaned version from https://raw.githubusercontent.com/cran/hsdar/master/man/vegindex.Rd
+
+    df <- data.table::fread("inst/hsdar-veg-indices.tex",
+      sep = "&",
+      data.table = FALSE, header = FALSE
+    ) %>%
+      setNames(nm = c("index", "wavelengths", "reference")) %>%
+      dplyr::mutate(wavel_ext = stringr::str_extract_all(wavelengths,
+        pattern = "[[:digit:]]+"
+      ))
+
+    vec_wavelengths <- df %>%
+      dplyr::pull(wavel_ext)
+
+    vec_wavelengths_num <- purrr::map(vec_wavelengths, ~ as.numeric(.x))
+
+    vec_wavelengths_num_clean <- purrr::map(
+      vec_wavelengths_num,
+      function(.x) .x[.x > 200]
+    ) %>%
+      purrr::set_names(df$index)
+
+    vec_wavelengths_num_final <- purrr::discard(
+      vec_wavelengths_num_clean,
+      function(.x) any(.x > 1000)
+    )
+
+    # convert list of vector to data.frame
+    df_wide <- plyr::ldply(vec_wavelengths_num_final, rbind)
+
+    df_wide_formatted <- df_wide %>%
+      tidyr::pivot_longer(
+        values_to = "wavelength",
+        cols = -tidyselect::one_of(".id")
+      ) %>%
+      dplyr::select(-name) %>%
+      dplyr::rename(class = .id) %>%
+      na.omit() %>%
+      dplyr::distinct()
+
+    return(df_wide_formatted)
+  })
 )
