9464cd3279c14dcbe5514b2bb9530c54b6419275
pat-s <patrick.schratz@gmail.com>
2020-04-18 22:38:37 +0200

    clean aggregation

--- code/081-aggregate_buffer2.R
+++ code/081-aggregate_buffer2.R
@@ -1,17 +1,7 @@
 bm_aggregated_plan_buffer2 <- drake_plan(
-  bm_aggregated_temp1_buffer2 = target(
+  bm_aggregated_new_buffer2 = target(
     mergeBenchmarkResults(
       benchmark_no_models_new_buffer2
     )
-  ),
-  bm_aggregated_temp2_buffer2 = target(
-    mergeBenchmarkResults(
-      benchmark_no_models_new_penalized_buffer2
-    )
-  ),
-  bm_aggregated_new_buffer2 = target(
-    mergeBenchmarkResults(list(
-      bm_aggregated_temp1_buffer2, bm_aggregated_temp2_buffer2
-    ))
   )
 )
