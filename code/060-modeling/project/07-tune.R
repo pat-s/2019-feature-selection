57170591a6d5dbf31e2f1a188304972c538380e6
pat-s <patrick.schratz@gmail.com>
2020-12-25 09:51:59 +0100

    [project]: fix task and tune parts

--- code/060-modeling/project/07-tune.R
+++ code/060-modeling/project/07-tune.R
@@ -1,7 +1,7 @@
 tune_xgboost_plan_project <- drake_plan(
   tune_xgboost = target(
     tune_wrapper(
-      learner = lrn_xgboost,
+      learner = lrn_xgboost_proj,
       tune.control = tune_ctrl_xgboost, show.info = TRUE,
       par.set = ps_xgboost, resampling = resampl_inner,
       measure = list(rmse), task = task_7_most_imp
