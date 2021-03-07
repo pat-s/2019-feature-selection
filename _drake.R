d35b16f4748b58293d1b957b96f7bf346f922313
pat-s <patrick.schratz@gmail.com>
2021-03-07 16:51:10 +0100

    prettify drake.R

--- _drake.R
+++ _drake.R
@@ -1,6 +1,9 @@
-# load packages ----------------------------------------------------------------
+# load R packages --------------------------------------------------------------
 
 source("code/099-packages.R")
+
+# create log directory
+fs::dir_create("log")
 
 library("drake")
 library("magrittr")
@@ -10,49 +13,30 @@ conflict_prefer("pull", "dplyr", quiet = TRUE)
 conflict_prefer("filter", "dplyr", quiet = TRUE)
 suppressMessages(library("R.utils"))
 
-options(clustermq.scheduler = "slurm")
-# suppressPackageStartupMessages(library(future.clustermq))
-# future::plan("clustermq")
-
 Sys.setenv(DISPLAY = ":99")
+
+options(
+  # set this to "slurm" if you have access to a Slurm cluster
+  clustermq.scheduler = "multicore",
+  clustermq.template = "~/papers/2019-feature-selection/slurm_clustermq.tmpl"
+)
 
 # load mlr extra learner -------------------------------------------------------
 
-source("https://raw.githubusercontent.com/mlr-org/mlr-extralearner/master/R/RLearner_regr_ranger_mtry_pow.R")
+source("https://raw.githubusercontent.com/mlr-org/mlr-extralearner/master/R/RLearner_regr_ranger_mtry_pow.R") # nolint
 
 # source functions -------------------------------------------------------------
 
-sourceDirectory("R")
-sourceDirectory("code")
+R.utils::sourceDirectory("R")
+R.utils::sourceDirectory("code")
 
-# Combine all ------------------------------------------------------------------
-
-plan_project <- bind_plans(
-  download_data_plan,
-  hyperspectral_processing_plan,
-  sentinel_processing_plan,
-  data_preprocessing_plan,
-
-  learner_project_plan,
-  ps_project_plan,
-  resampling_plan_project,
-  tasks_plan,
-  task_project_plan,
-
-  tune_ctrl_xgboost_project,
-  tune_xgboost_plan_project,
-  train_xgboost_plan_project,
-
-  reports_plan_project
-)
+# Combine all plans ------------------------------------------------------------
 
 plan_paper <- bind_plans(
   download_data_plan,
   hyperspectral_processing_plan,
-  # sentinel_processing_plan,
 
   data_preprocessing_plan,
-
   tasks_plan,
 
   filter_eda_plan,
@@ -64,61 +48,19 @@ plan_paper <- bind_plans(
   tune_wrapper_plan,
 
   benchmark_plan,
-
   bm_aggregated_plan,
 
-  # train_plan,
   feature_imp_plan,
   reports_plan_paper
 )
 
-options(
-  clustermq.scheduler = "slurm",
-  clustermq.template = "~/papers/2019-feature-selection/slurm_clustermq.tmpl"
-)
-
-# plan_project %<>% dplyr::mutate(stage = as.factor(stage))
-# plan_paper %<>% dplyr::mutate(stage = as.factor(stage))
-
-
-# project ----------------------------------------------------------------------
-
-# drake_config(plan_project,
-#   # targets = "eda_wfr",
-#   verbose = 1, lazy_load = "eager",
-#   log_make = "log/drake-project.log",
-#   caching = "worker",
-#   template = list(
-#     log_file = "log/worker-project%a.log",
-#     n_cpus = 4,
-#     memory = 20000,
-#     partition = "all",
-#     job_name = "pred-defol"
-#   ),
-#   prework = list(
-#     quote(load_packages()),
-#     quote(set.seed(1, "L'Ecuyer-CMRG")),
-#     quote(future::plan(future::multisession, workers = 2))
-#     # quote(parallelStart(
-#     #   mode = "multicore",
-#     #   cpus = 12,
-#     #   level = "mlr.resample"
-#     # ))
-#   ),
-#   garbage_collection = TRUE,
-#   jobs = 4,
-#   parallelism = "clustermq",
-#   lock_envir = FALSE,
-#   keep_going = FALSE
-# )
-
-# paper -----------------------------------------------------------------------
+# paper ------------------------------------------------------------------------
 
 # config for long running tasks
 drake_config(plan_paper,
    targets = c(
-     c("tree_per_tree", "veg_indices", "nri_indices")
-     # c("task_new_buffer07_reduced_cor")
+     # replace this with the target name that should be computed
+     c("task_reduced_cor")
    ),
   verbose = 1,
   lazy_load = "eager",
@@ -126,54 +68,25 @@ drake_config(plan_paper,
   log_make = "log/drake-BM.log",
   caching = "main",
   template = list(
-    # 60 unique workers with 4 cores/3.5G RAM on partition c0-c5
     log_file = "log/worker-BM%a.log", n_cpus = 4,
     memory = 3500, job_name = "paper2-BM",
     partition = "all"
   ),
   prework = list(
     quote(load_packages()),
-    # quote(future::plan(callr, workers = 4)),
+    # seed for parallel tasks
     quote(set.seed(1, "L'Ecuyer-CMRG")),
+    # intra-target parallelization (when running CV with mlr)
     quote(parallelStart(
       mode = "multicore",
       cpus = 4,
       level = "mlr.resample",
-      # level = "mlr.selectFeatures", # for MC feature selection
+      # level = "mlr.selectFeatures", # for mlr feature importance calculation
       mc.cleanup = TRUE,
       mc.preschedule = FALSE
     ))
   ),
   garbage_collection = TRUE,
-  jobs = 3, parallelism = "clustermq",
+  jobs = 4, parallelism = "clustermq",
   keep_going = FALSE, recover = FALSE, lock_envir = FALSE, lock_cache = FALSE
 )
-
-# config for sequential, quick tasks
-# drake_config(plan_paper,
-#   targets = c("eval_performance_wfr", "spectral_signatures_wfr", "filter_correlation_wfr"),
-#   verbose = 1,
-#   lazy_load = "eager",
-#   packages = NULL,
-#   log_make = "log/drake2.log",
-#   caching = "main",
-#   template = list(
-#     log_file = "log/worker-single%a.log",
-#     n_cpus = 4, memory = 4000,
-#     job_name = "paper2-single", partition = "all"
-#   ),
-#   prework = list(
-#     quote(load_packages()),
-#     # quote(future::plan(callr, workers = 4)),
-#     quote(set.seed(1, "L'Ecuyer-CMRG"))#,
-#     # quote(parallelStart(
-#     #   mode = "multicore",
-#     #   cpus = 4,
-#     #   level = "mlr.resample",
-#     #   mc.cleanup = TRUE,
-#     #   mc.preschedule = FALSE
-#     # ))
-#   ),
-#   garbage_collection = TRUE, jobs = 3, parallelism = "clustermq",
-#   keep_going = FALSE, recover = FALSE, lock_envir = TRUE, lock_cache = FALSE
-# )
